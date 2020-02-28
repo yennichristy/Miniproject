b4f01ebfe45160efdf483970b7a3cd6f6d8276c0
Yenni Permatasari <yennikristianip@gmail.com>
2020-02-28 14:48:47 +0700

    [Yenni] - Finishing Footer & Update class name into BEM

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,60 +9,72 @@ import "../assets/styles/Footer.scss";
 
 const Footer = () => {
   return (
-    <div className="footer">
-      <div className="footer__brand">
-        <div>
-          <img src={logo} alt="logo" width="20%" height="20%"></img>
-          <h4>MilanTV</h4>
+    <React.Fragment>
+      <div className="footer">
+        <div className="footer__brand">
+          <div className="footer__brand__logo">
+            <img src={logo} alt="logo" width="20%" height="20%"></img>
+            <h4>MilanTV</h4>
+          </div>
+          <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard.printing and
+            typesetting industry. Lorem Ipsum has been the industry's standard
+          </p>
         </div>
-        <p>
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard.printing and
-          typesetting industry. Lorem Ipsum has been the industry's standard
-        </p>
-      </div>
-      <div className="footer__link">
-        <li>
-          <ul>
-            <p>About us</p>
-            <p>Blog</p>
-            <p>Services</p>
-            <p>Career</p>
-            <p>Media Center</p>
-          </ul>
-        </li>
-      </div>
-      <div className="footer__media">
-        <h5>Download</h5>
-        <img className="footer--playstore" src={pstore} alt="Play Store"></img>
-        <img className="footer--appstore" src={astore} alt="Apple Store"></img>
-        <div>
-          <h5>Social Media</h5>
-          <img
-            className="footer__social-media"
-            src={fb}
-            alt="Facebook"
-            width="5%"
-            height="5%"
-          ></img>
-          <img
-            className="footer__social-media"
-            src={pin}
-            alt="Pinterest"
-            width="5%"
-            height="5%"
-          ></img>
-          <img
-            className="footer__social-media"
-            src={ig}
-            alt="Instagram"
-            width="5%"
-            height="5%"
-          ></img>
+        <div className="footer__link">
+          <li>
+            <ul>
+              <p>About us</p>
+              <p>Blog</p>
+              <p>Services</p>
+              <p>Career</p>
+              <p>Media Center</p>
+            </ul>
+          </li>
         </div>
+        <div className="footer__media">
+          <h5>Download</h5>
+          <img
+            className="footer__media__store"
+            src={pstore}
+            alt="Play Store"
+          ></img>
+          <img
+            className="footer__media__store"
+            src={astore}
+            alt="Apple Store"
+          ></img>
+          <div>
+            <h5>Social Media</h5>
+            <img
+              className="footer__media__social-media"
+              src={fb}
+              alt="Facebook"
+              width="5%"
+              height="5%"
+            ></img>
+            <img
+              className="footer__media__social-media"
+              src={pin}
+              alt="Pinterest"
+              width="5%"
+              height="5%"
+            ></img>
+            <img
+              className="footer__media__social-media"
+              src={ig}
+              alt="Instagram"
+              width="5%"
+              height="5%"
+            ></img>
+          </div>
+        </div>
+      </div>
+      <div className="copyright">
         <p>Copyright © 2000-202 MilanTV. All Rights Reserved</p>
       </div>
-    </div>
+    </React.Fragment>
   );
 };
 
