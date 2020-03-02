c0118cfa09f1b5712c081d4f515cf5df9df8228d
Yenni Permatasari <yennikristianip@gmail.com>
2020-03-02 09:51:22 +0700

    [Yenni] - New layout banner

---
+++ src/components/layouts/Banner.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Banner = () => {
+  return (
+    <div className="banner">
+      <div>
+        <h4>Title</h4>
+        <p>*****</p>
+        <p>2200 review</p>
+        <p>Description</p>
+        <button>Watch Trailer</button>
+        <button>Add to Watchhlist</button>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
