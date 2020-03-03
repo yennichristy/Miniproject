e9170f7182e81912731d34e7c0f4c88210a6b0ed
Yenni Permatasari <yennikristianip@gmail.com>
2020-03-03 09:29:04 +0700

    [Yenni] - New page cast page

---
+++ src/components/pages/CastsPage.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Header from "../layouts/Header";
+import Banner from "../layouts/Banner";
+import Button from "../layouts/AboutMovie";
+import Casts from "../layouts/Casts";
+import Footer from "../layouts/Footer";
+
+const CastPage = () => {
+  return (
+    <div>
+      <Header />
+      <Banner />
+      <Button />
+      <Casts />
+      <Footer />
+    </div>
+  );
+};
+
+export default CastPage;
